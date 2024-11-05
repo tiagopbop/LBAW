12e9e9560a70f66b253f1e75d7d807b918115929
Tiago de Pinho Bastos de Oliveira Pinheiro <up202207890@edu.fe.up.pt>
2024-11-05 23:47:18 +0000

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -21,9 +21,9 @@ VALUES
 -- Populate project table
 INSERT INTO project (project_id, availability, project_creation_date, archived_status, project_title, project_description)
 VALUES
-    (1, TRUE, CURRENT_DATE, FALSE, "Ovelha", "Branco"),
-    (2, TRUE, CURRENT_DATE, FALSE, "Abelha", "Amarelo"),
-    (3, FALSE, CURRENT_DATE, TRUE, "Àrvore", "Castanho");
+    (1, TRUE, CURRENT_DATE, FALSE, 'Ovelha', 'Branco'),
+    (2, TRUE, CURRENT_DATE, FALSE, 'Abelha', 'Amarelo'),
+    (3, FALSE, CURRENT_DATE, TRUE, 'Àrvore', 'Castanho');
 
 -- Populate task table
 INSERT INTO task (project_id, task_name, status, details, assigned_to, due_date, priority, created_at, updated_at)
