ed89ffe808626b7a4b96939198a92bdc3cbeca91
Diogo Paulo Gomes Dias Leite Neves <up202108460@edu.fe.up.pt>
2024-11-24 19:00:13 +0000

    Update file Favorited.php

--- app/Models/Favorited.php
+++ app/Models/Favorited.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Favorite extends Model
+class Favorited extends Model
 {
     use HasFactory;
 
