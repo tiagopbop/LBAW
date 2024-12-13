36a901fac0d690a056a670ade9ee8c7292d4f608
Rafaelcostapires <up202208602@up.pt>
2024-12-13 21:44:39 +0000

    mytasksupdated

--- resources/views/pages/mytasks.blade.php
+++ resources/views/pages/mytasks.blade.php
@@ -10,9 +10,20 @@
     @else
         <ul>
             @foreach ($tasks as $task)
-                <li>{{ $task->task_name }}</li> <!-- Assuming 'task_name' exists in the 'task' table -->
+                <div class="task" data-id="{{ $task->task_id }}" style="text-align: left; margin-top: 20px;">
+                            <p><strong>Title:</strong> {{ $task->task_name }}</p>
+                            <p><strong>Status:</strong> {{ $task->status }}</p>
+                            <p><strong>Due date:</strong> {{ $task->due_date }}</p>
+                            <p><strong>Assigned To:</strong>
+                                @if($task->assignedUsers && $task->assignedUsers->isNotEmpty())
+                                    {{ $task->assignedUsers->pluck('username')->join(', ') }}
+                                @else
+                                    Not assigned
+                                @endif
+                            </p>
             @endforeach
         </ul>
+        
     @endif
 </div>
 @endsection
